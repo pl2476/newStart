c6519a24c53b34d94f177149342f31f47eb8a55c
Light <pl2476@126.com>
2019-05-17 17:19:35 +0800

    update demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Hello from './components/StatefulHello'
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Hello name="Hello" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
